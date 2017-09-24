7b052322cc75b5b8d53cef81f5eb172db582d81c
teacherigor <teacherigor6@gmail.com>
2017-09-24 11:01:13 +0300

    added Get Price button to the html file and jQ functions append price and remove button to the jQ file

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function() {
-	$ ("#destinations li:even"). text("Khmelnitskiy")
+	var price = $('<p>From $399.99</p>');
+	$('.america').append(price).price.appendTo($('.america'));
+	$('button').remove();
 });
