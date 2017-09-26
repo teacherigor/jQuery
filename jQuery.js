a6a30c247e66b5641c43b40af4b2191b37e2f14b
teacherigor <teacherigor6@gmail.com>
2017-09-26 22:50:48 +0300

    added closing brace, paren and semicolon to jQ code

--- jQuery.js
+++ jQuery.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('button').on('click', function() {
-		//run this function on click
-	});
+	var price = $('<p>From $399.99</p>');
+	$('.book').before(price);
+	$('button').remove();
+});
 });
