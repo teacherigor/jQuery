e73d3fcec3cb0d441fd409582688bf81188f5085
Igor Semenuk <teacherigor6@gmail.com>
2017-09-27 09:59:04 +0300

    changed the click handler to trigger when the .tour list item is clicked instead

--- jQuery.js
+++ jQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('button').on('click', function() {
+	$('.america').on('click', function() {
 	var price = $('<p>From $399.99</p>');
 	$('.book').before(price);
 	$('button').remove();
