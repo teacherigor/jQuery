77e7e0da19566dfe75f8e39476abb466063fcf73
Igor Semenuk <teacherigor6@gmail.com>
2017-09-28 15:07:22 +0300

    added .closest method to $(this) line

--- jQuery.js
+++ jQuery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('button').on('click', function() {
 	var price = $('<p>From $399.99</p>');
-	$(this).after(price);
+	$(this).closest('.vacation').append(price);
 	$(this).remove();
 });
 });
