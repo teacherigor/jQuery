3a858aa9249f4ed26be622f9f554bbc53ddca171
Igor Semenuk <teacherigor6@gmail.com>
2017-09-28 14:08:51 +0300

    replaced '.vacation' selector with 'button' selector

--- jQuery.js
+++ jQuery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('.vacation').on('click', function() {
+	$('button').on('click', function() {
 	var price = $('<p>From $399.99</p>');
-	$('.book').before(price);
+	$('.book').append(price);
 	$(this).remove();
 });
 });
